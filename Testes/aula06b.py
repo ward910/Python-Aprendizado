05a538eb3b8f61422098bffbde7e5de35035e2c4
vinicius barbosa de aguiar <69726088+ward910@users.noreply.github.com>
2021-08-07 16:04:03 -0300

    update

---
+++ Testes/aula06b.py
@@ -0,0 +1,2 @@
+n = (input('Digite um número '))
+print(n.isalnum())
--- Testes/input.py
+++
@@ -1,5 +0,0 @@
-nome = input('Seu nome ')
-idade = input('Sua idade ')
-peso = input('Seu peso ')
-
-print(f'Seu nome é {nome} sua idade é {idade} e peso {peso}')
