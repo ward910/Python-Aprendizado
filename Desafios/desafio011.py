e98ae0fdaaf2afcc76fc9f7c77072dc12ad92df3
vinicius barbosa de aguiar <69726088+ward910@users.noreply.github.com>
2021-08-15 14:01:54 -0300

    Update desafio011.py

---
+++ Desafios/desafio011.py
@@ -0,0 +1,8 @@
+larg = float(input('Lagura da parade: '))
+altu = float(input('Altura da parade: '))
+
+area = larg * altu
+tinta = area / 2
+
+print(f'Sua parede tem a dimesão {larg}x{altu} e tem a area {area}m²')
+print(f'Para pintar essa parede você precisará de {tinta:.1f}l de tinta')
