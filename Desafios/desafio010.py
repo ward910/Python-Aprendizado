5bbc0edde468d1f27ce9038a07052c75f532da65
vinicius barbosa de aguiar <69726088+ward910@users.noreply.github.com>
2021-08-15 13:26:10 -0300

    Update desafio010.py

---
+++ Desafios/desafio010.py
@@ -0,0 +1,4 @@
+dinheiroReais = float(input('Quanto dinheiro você tem na carteira? R$'))
+contaDolar = 5.00
+
+print(f'Com R${dinheiroReais:.2f} você pode comprar US${dinheiroReais / contaDolar:.2f}')
