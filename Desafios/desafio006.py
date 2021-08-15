64c6605eacefa765f6e31f2a927fc5ff3cd990e0
vinicius barbosa de aguiar <69726088+ward910@users.noreply.github.com>
2021-08-15 11:00:53 -0300

    Update desafio006.py

--- Desafios/desafio006.py
+++ Desafios/desafio006.py
@@ -4,4 +4,4 @@ num = int(input('Digite um número '))
 
 print(f'Dobro é {num * 2}')
 print(f'Triplo é {num * 3}')
-print(f'A raiz quadrada {sqrt(num):.3f}')
+print(f'A raiz quadrada {sqrt(num):.2f}')
