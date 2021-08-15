67f40442ff14854d76ddb54429a20bd0f997e33c
vinicius barbosa de aguiar <69726088+ward910@users.noreply.github.com>
2021-08-15 12:45:25 -0300

    Update desafio008.py

---
+++ Desafios/desafio008.py
@@ -0,0 +1,10 @@
+m = float(input('Um número em metros: '))
+
+print(f'A medida de {m} corresponde:')
+
+print(f'{m / 1000}km')
+print(f'{m / 100}hm')
+print(f'{m / 10}dam')
+print(f'{m * 10:.0f}dm')
+print(f'{m * 100:.0f}cm')
+print(f'{m * 1000:.0f}mm')
