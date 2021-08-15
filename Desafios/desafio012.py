3add0ae1195ff6823a6d727990e0d74d9d2d11ab
vinicius barbosa de aguiar <69726088+ward910@users.noreply.github.com>
2021-08-15 15:01:20 -0300

    Update desafio012.py

--- Desafios/desafio012.py
+++ Desafios/desafio012.py
@@ -1,4 +1,4 @@
-produto = float(input('Qual é o preço do produto: '))
+produto = float(input('Qual é o preço do produto: R$'))
 desconto = produto - (produto * (5 / 100))
 
 print(f'O produto que custava R${produto}, na promoção com desconto de 5% vai custa R${desconto:.2f}')
