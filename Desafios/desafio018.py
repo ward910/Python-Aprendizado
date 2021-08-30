9156bf744260a917247baca9defdf80002a86d05
Vinicius Barbosa de Aguiar <vb9763662@gmail.com>
2021-08-30 12:16:32 -0300

    all

--- Desafios/desafio018.py
+++ Desafios/desafio018.py
@@ -1,1 +1,8 @@
-
+from math import sin, cos, tan, radians
+an = float(input('Digite o ângulo que você deseja: '))
+sino = sin(radians(an))
+print(f'O ângulo de {an} tem o SENO de {sino:.2f}')
+consseno = cos(radians(an))
+print(f'O ângulo de {an} tem o CONSSENO de {consseno:.2f}')
+tangente = tan(radians(an))
+print(f'O ângulo de {an} tem o TANGENTE de {tangente:.2f}')
--- Desafios/desafio020.py
+++ Desafios/desafio020.py
@@ -1,9 +1,7 @@
-from random import choice
+from random import s
 
 aluno01 = str(input('Primeiro aluno: '))
 aluno02 = str(input('Segundo aluno: '))
 aluno03 = str(input('Terceiro aluno: '))
 aluno04 = str(input('Quarto aluno: '))
-lista = [aluno01, aluno02, aluno03, aluno04]
 
-print(f'O aluno escolhido foi {choice(lista)}')
