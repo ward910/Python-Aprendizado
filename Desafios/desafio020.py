c2073177d192da5bcfc2ee8a21940c90a5a927a2
Vinicius Barbosa de Aguiar <vb9763662@gmail.com>
2021-09-25 00:50:40 -0300

    mod

--- Desafios/desafio020.py
+++ Desafios/desafio020.py
@@ -5,3 +5,6 @@ aluno02 = str(input('Segundo aluno: '))
 aluno03 = str(input('Terceiro aluno: '))
 aluno04 = str(input('Quarto aluno: '))
 
+alunos = [aluno01, aluno02, aluno03, aluno04]
+shuffle(alunos)
+print(alunos)
---
+++ Desafios/desafio021.py
@@ -0,0 +1,3 @@
+from playsound import playsound
+
+playsound('./Assets/Audio.mp3', True)
