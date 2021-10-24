b35ba0b621e638f3fd48eb59c662e7ed214aa829
Vinicius Barbosa de Aguiar <vb9763662@gmail.com>
2021-10-24 15:08:42 -0300

    add aula010a.py

---
+++ Aulas/aula010a.py
@@ -0,0 +1,10 @@
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2)/2
+
+print(f'A sua média foi {m:.1f}')
+
+if (m >= 6):
+    print('PARABENS')
+else:
+    print('ESTUDE MAIS!')
