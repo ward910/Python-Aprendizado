9f2030de2b16f03f1f7e3d5a41edcc04bb047878
Vinicius Barbosa de Aguiar <vb9763662@gmail.com>
2021-11-14 18:34:46 -0300

    add Desafios/desafio035.py

---
+++ Desafios/desafio035.py
@@ -0,0 +1,12 @@
+print('-=' * 20)
+print('Analisador de triângulo')
+print('-=' * 20)
+
+a = float(input('Primeiro segemento: '))
+b = float(input('Segundo segemento: '))
+c = float(input('Terceiro segemento: '))
+
+if a + b > c and a + c > b and b + c > a:
+    print('Os segementos PORDEM FORMAR um triângulo!')
+else:
+    print('Os segementos NÃO PORDEM FORMAR um triângulo!')
