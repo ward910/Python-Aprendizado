296b4e1e8e51d014af5ef84f1f79bf6b2e1d6a63
Vincius Barbosa de Aguiar <vb9763662@gmail.com>
2021-11-22 11:24:22 -0300

    add Desafios/desafio038.py

---
+++ Desafios/desafio038.py
@@ -0,0 +1,9 @@
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+
+if n1 == n2:
+    print('Os dois valores são IGUAIS')
+elif n1 > n2:
+    print('O PRIMEIRO valor é maior')
+else:
+    print('O SEGUNDO valor é maior')
