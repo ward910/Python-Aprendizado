d88d02836ae11ea6edc85587e2465496d3f915c6
Vincius Barbosa de Aguiar <vb9763662@gmail.com>
2022-01-08 22:06:55 -0300

    add Desafios/desafio055.py

---
+++ Desafios/desafio055.py
@@ -0,0 +1,16 @@
+maior = 0
+menor = 0 
+
+for p in range(1, 6):
+    peso = float(input('Peso da {}° pessoa: '.format(p)))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+
+print(f'O maior peso lido foi de {maior}Kg')
+print(f'O menor peso lido foi de {menor}Kg') 
